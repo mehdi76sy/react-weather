f64150aa33859c1b301eb0e8eb1d6e0d11a31b97
Mehdi Akbari <www.mehdi76mesy@gmail.com>
2022-10-27 20:58:43 +0200

    Create search component and setting up weather api

---
+++ src/api.js
@@ -0,0 +1,27 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'esGbwrm390mshS2BC10RALxQRtZTp1W7sFMjsnyJ1JzDXVKW0H',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '751539bc8cmsh605722ca4cdbcb2p162368jsn5074efcc5ea7',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+
+
