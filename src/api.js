f366168b9640e9769bb03db891525ea9ea134683
Mehdi Akbari <www.mehdi76mesy@gmail.com>
2022-10-31 15:27:14 +0100

    Add weather forecast, styling the UI and debugging api problems

--- src/api.js
+++ src/api.js
@@ -1,27 +1,12 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'esGbwrm390mshS2BC10RALxQRtZTp1W7sFMjsnyJ1JzDXVKW0H',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+		'X-RapidAPI-Key': ;)
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+	},
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
-
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '751539bc8cmsh605722ca4cdbcb2p162368jsn5074efcc5ea7',
-// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-
-
-
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = ;)
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -13,28 +13,30 @@ const Search = ({ onSearchChange }) => {
       .then((response) => response.json())
       .then((response) => {
         return {
-          options: response.date.map((city) => {
+          options: response.data.map((city) => {
             return {
               value: `${city.latitude} ${city.longitude}`,
               label: `${city.name}, ${city.countryCode}`,
-            }
-          })
-        }
-      })
-      .catch((err) => console.error(err));
+            };
+          }),
+        };
+      });
+      // .catch((err) => console.error(err));
   };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
   };
+
+
   return (
     <AsyncPaginate
       placeholder="Search for city"
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
-      loadingOptions={loadOptions}
+      loadOptions={loadOptions}
     />
   );
 };
